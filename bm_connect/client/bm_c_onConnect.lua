cdec8090871ca27789ac201ddef434275ece6c9e
Julien Savoie <juliensavoie7@gmail.com>
2020-06-16 17:22:39 -0400

    Character Select starts in clouds

--- bm_connect/client/bm_c_onConnect.lua
+++ bm_connect/client/bm_c_onConnect.lua
@@ -1,15 +1,42 @@
 AddEventHandler('onClientGameTypeStart', function()
     local playerServerID = GetPlayerServerId(PlayerId())
+    DisplayRadar(false)
     SwitchOutPlayer(PlayerPedId(), 1, 2)
-    while GetPlayerSwitchState() ~= 5 do
-        Citizen.Wait(0)
-    end
-    TriggerServerEvent("bm:playerConnect", playerServerID)
-    SendNUIMessage({
-        type = "character_selection_open"
-    })
+    
 
-    SetNuiFocus(true, true)
-    ShutdownLoadingScreenNui()
+    Citizen.CreateThread(function()
+    	while GetPlayerSwitchState() ~= 4 do
+	        Citizen.Wait(0)
+	    end
+    
+	    TriggerServerEvent("bm:playerConnect", playerServerID)
 
+	    ShutdownLoadingScreenNui()
+
+	    SendNUIMessage({
+	        type = "character_selection_open"
+	    })
+
+	    SetNuiFocus(true, true)
+
+	end)
+
+	
+
+end)
+
+RegisterNUICallback('bm:start', function(data, cb)
+
+	SwitchInPlayer(PlayerPedId())
+	SetNuiFocus(false, false)
+	
+	Citizen.CreateThread(function()
+		while IsPlayerSwitchInProgress() do
+			Citizen.Wait(0)
+		end
+		DisplayRadar(true)
+	end)
+	
+
+    cb('ok')
 end)
